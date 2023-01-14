ceea9e32ec4e0610cbebb5482044084a78e2a8a7
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 22:23:35 +0700

    add button method, dan table create data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
-export default{
-  data(){
-    return{
-      suhu: []
+export default {
+  data() {
+    return {
+      newData: {
+        waktu: "",
+        suhu: []
+      }
     }
   },
-  methods:{
+  methods: {
     async getData() {
       try {
         const response = await fetch("http://localhost:3000/sensor/suhu");
@@ -15,6 +18,17 @@ export default{
         console.error(error);
       }
     },
+    async createData() {
+      try {
+        await axios.post("http://localhost:3000/sensor/suhu", this.newData);
+        this.newData = { waktu: "", suhu: "" };
+        this.$emit("data-created");
+        alert("Data berhasil ditambahkan!");
+      } catch (error) {
+        console.error(error);
+        alert("Terjadi kesalahan saat menambahkan data.");
+      }
+    }
   },
 };
 </script>
@@ -38,4 +52,14 @@ export default{
   <div class="text-center">
     <button class="bg-red-500 text-white rounded-3xl p-2" @click="getData()">Ambil Data</button>
   </div>
+
+  <form @submit.prevent="createData">
+    <label for="waktu">Waktu:</label>
+    <input type="text" v-model="newData.waktu" id="waktu" name="waktu">
+
+    <label for="suhu">Suhu:</label>
+    <input type="text" v-model="newData.suhu" id="suhu" name="suhu">
+
+    <button type="submit" class="bg-blue-500 text-white rounded-3xl p-2">Create</button>
+  </form>
 </template>
